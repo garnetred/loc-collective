93bdd3c9288dca813f4c59b06a842227ff1002f2
December G <dgarnetsmith@gmail.com>
2020-06-10 16:27:38 -0600

    Add active styling for links

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,22 +1,29 @@
-import React from 'react';
-import {NavLink} from 'react-router-dom';
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
 import "./Header.css";
 
-
 const Header = () => {
-    return(
-        <nav>
-            <NavLink className="loc-collective" to="/">
-                loc collective.
-            </NavLink>
-            <section className="header-link-container">
-                <NavLink className='nav-items' to="/about">about</NavLink>
-                <NavLink className='nav-items' to="/contact">contact</NavLink>
-                <NavLink className='nav-items' to="/styles">styles</NavLink>
-                <NavLink className='nav-items' to="/resources">resources</NavLink>
-            </section>
-        </nav>
-    )
-}
+  return (
+    <nav>
+      <Link className="loc-collective" to="/">
+        loc collective.
+      </Link>
+      <section className="header-link-container">
+        <NavLink className="nav-items" to="/about">
+          about
+        </NavLink>
+        <NavLink className="nav-items" to="/contact">
+          contact
+        </NavLink>
+        <NavLink className="nav-items" to="/styles">
+          styles
+        </NavLink>
+        <NavLink className="nav-items" to="/resources">
+          resources
+        </NavLink>
+      </section>
+    </nav>
+  );
+};
 
 export default Header;
