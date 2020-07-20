e2de0de57deb19853aff277a12459c762032d9af
December G <dgarnetsmith@gmail.com>
2020-07-20 08:51:49 -0600

    Update hairstyles page to include descriptions

--- src/content-data.js
+++ src/content-data.js
@@ -1,30 +1,54 @@
 export const styles = [
-
-  { header: "Interlocks", info: "Interlocks are a hairstyle." },
-  { header: "Sisterlocks", info: "Sisterlocks are a hairstyle." },
-  { header: "Brotherlocks", info: "Brotherlocks are a hairstyle." },
-  { header: "Microlocs", info: "Microlocs are a hairstyle." },
-  { header: "Faux locs", info: "Faux locs are a hairstyle." },
-  { header: "Traditional locs", info: "Traditional locs are a hairstyle." },
+  {
+    header: "Interlocks",
+    info:
+      "Interlocks are generally smaller locs that are maintained and started via a method called interlocking, which crochets the hair together. Some people choose interlocks because retightenings often last longer and you can do things such as swim or wash your hair immediately after a retightening with no ill effect. Interlocking can also be used to maintain other types of locs as well.",
+  },
+  {
+    header: "Sisterlocks",
+    info:
+      "Sisterlocks are micro locs (very small locs) They come in several different sizes. Since sisterlocks are a patented technique, they can be expensive to install and maintain, but they offer a lot of versatility in terms of hair styling options and the same benefits as interlocking above.",
+  },
+  {
+    header: "Brotherlocks",
+    info:
+      "Brotherlocks are larger sisterlocks - they may not always be considered microlocs. They're also a patented technique and look similar to sisterlocks except for the size.",
+  },
+  {
+    header: "Microlocs",
+    info:
+      "Microlocs very small locs. Someone with microlocs can have upwards of 500+ locs. Usually microlocs are not maintained by palm rolling.",
+  },
+  {
+    header: "Faux locs",
+    info:
+      "Faux locs hair extensions that look like locs. They often look like traditional locs, but have a shinier appearance. This can be a good way to test out locs without making the commitment.",
+  },
+  {
+    header: "Traditional locs",
+    info:
+      "Traditional locs are a long-term hair style that can be started using many methods - most popularly, twists or comb coils. Traditional locs are normally retwisted using palm rolling - however, it's possible to get traditional locs and then switch to a different retightening technique. Traditional locs are larger than other locs and someone with traditional locs will have fewer locs than someone with micro locs.",
+  },
 ];
 
 export const resources = [
   {
     header: "Cultural Appropriation",
-    info: ["another link", 'some more content'],
+    info: ["another link", "some more content"],
   },
   {
     header: "Black Hair Care",
-    info: ['some other link', 'something else'],
+    info: ["some other link", "something else"],
   },
 ];
 
-//how do I display the resources if this doesn't work?
-
 export const about = [
-  { info:
-      "This site was designed to make the process of finding a loctician much easier for black people. In my own search for a loctician, I often noticed that it was difficult to filter out stylists who couldn’t do black hair or find stylists who specialized in locs other than traditional locs. It was even difficult to find people to start locs in less common ways. This site tries to make this process a little easier for others. "},
-    {info:
-      "If you have any questions, comments, or additional concerns, please feel free to reach out via the contact form, available here. You can also view my other work via GitHub. ",
+  {
+    info:
+      "This site was designed to make the process of finding a loctician much easier for black users with locs. In my own search for a loctician, I often noticed that it was difficult to filter out stylists who couldn’t do black hair or find stylists who specialized in locs other than traditional locs. It was even difficult to find people to start locs in less common ways. This site tries to make this process a little easier for others. ",
+  },
+  {
+    info:
+      "If you have any questions, comments, or additional concerns, please feel free to reach out via the contact form, available here. You can also view my other work via GitHub @garnetred.",
   },
 ];
