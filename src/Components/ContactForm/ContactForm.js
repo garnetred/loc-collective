db3dd1186f79541d7fbc887c28d175215fa97d67
December G <dgarnetsmith@gmail.com>
2022-07-04 20:27:14 -0600

    Switch contact form to functional component

--- src/Components/ContactForm/ContactForm.js
+++ src/Components/ContactForm/ContactForm.js
@@ -1,32 +1,27 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import "./ContactForm.css";
 
-class ContactForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "",
-      email: "",
-      message: "",
-    };
-  }
+const ContactForm = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
-  handleChange = (e) => {
+  const handleChange = (e) => {
     const { name } = e.target;
     if (name === "name") {
-      this.setState({ name: e.target.value });
+      setName(e.target.value);
     }
     if (name === "email") {
-      this.setState({ email: e.target.value });
+      setEmail(e.target.value);
     }
 
     if (name === "message") {
-      this.setState({ message: e.target.value });
+      setMessage(e.target.value);
     }
   };
 
-  submitForm = (e) => {
+  const submitForm = (e) => {
     e.preventDefault();
     // if (this.state.location !== "" && this.state.style !== "") {
     //   this.props.retrieveSearchResults(this.state);
@@ -36,7 +31,7 @@ class ContactForm extends Component {
     // }
   };
 
-  displayErrorMessage = () => {
+  const displayErrorMessage = () => {
     // if (this.state.location === "") {
     //   this.setState({ locationError: true });
     // }
@@ -45,68 +40,59 @@ class ContactForm extends Component {
     // }
   };
 
-  render() {
-    return (
-      <section className="contact-form-container">
-        <form
-          className="contact-form"
-          data-testid="contactform"
-          action="mailto:test@gmail.com"
-          method="post"
+  return (
+    <section className="contact-form-container">
+      <form
+        className="contact-form"
+        data-testid="contactform"
+        action="mailto:test@gmail.com"
+        method="post"
+      >
+        <p className="contact-form-labels">Name:</p>
+        <input
+          className="name-input"
+          type="text"
+          placeholder="name"
+          name="name"
+          aria-label="name"
+          value={name}
+          onChange={(e) => handleChange(e)}
+          required
+        />
+        <p className="contact-form-labels">Email:</p>
+        <input
+          className="email-input"
+          type="text"
+          placeholder="email"
+          name="email"
+          aria-label="email"
+          value={email}
+          onChange={(e) => handleChange(e)}
+          required
+        />
+        <p className="contact-form-labels">Comments:</p>
+        <textarea
+          className="message-input"
+          type="textarea"
+          placeholder="message"
+          name="message"
+          aria-label="message"
+          value={message}
+          onChange={(e) => handleChange(e)}
+          required
+        />
+        <button
+          type="submit"
+          className="contact-form-button"
+          onClick={submitForm}
         >
-          <p className="contact-form-labels">Name:</p>
-          <input
-            className="name-input"
-            type="text"
-            placeholder="name"
-            name="name"
-            aria-label="name"
-            value={this.state.name}
-            onChange={(e) => this.handleChange(e)}
-            required
-          />
-          <p className="contact-form-labels">Email:</p>
-          <input
-            className="email-input"
-            type="text"
-            placeholder="email"
-            name="email"
-            aria-label="email"
-            value={this.state.email}
-            onChange={(e) => this.handleChange(e)}
-            required
-          />
-          <p className="contact-form-labels">Comments:</p>
-          <textarea
-            className="message-input"
-            type="textarea"
-            placeholder="message"
-            name="message"
-            aria-label="message"
-            value={this.state.message}
-            onChange={(e) => this.handleChange(e)}
-            required
-          />
-          <button
-            type="submit"
-            className="contact-form-button"
-            onClick={this.submitForm}
-          >
-            submit
-          </button>
-        </form>
-        <section className="error-wrapper">
-          {this.state.styleError && (
-            <p className="form-error-message">Please choose a hairstyle.</p>
-          )}
-          {this.state.locationError && (
-            <p className="form-error-message">Please input a location.</p>
-          )}
-        </section>
-      </section>
-    );
-  }
-}
+          submit
+        </button>
+      </form>
+      <section className="error-wrapper"></section>
+    </section>
+  );
+};
 
 // ContactForm.propTypes = {
 //   retrieveSearchResults: PropTypes.func,
