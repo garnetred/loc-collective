5a03854a1964b7461a9997b145ebc5adaa138b9e
December G <dgarnetsmith@gmail.com>
2022-07-29 15:38:15 -0600

    Create new environment variables

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,29 +1,26 @@
 export const fetchStylist = async (id) => {
-  let myHeaders = new Headers();
-  myHeaders.append("Authorization", `Bearer ${process.env.REACT_APP_API_KEY}`);
-  myHeaders.append("Content-Type", "application/json");
-
-  let requestOptions = {
-    method: "GET",
-    headers: myHeaders,
-  };
+  // need to tell if we're in production or not
+  const url =
+    process.env.NODE_ENV !== "production"
+      ? process.env.REACT_APP_DEV_API_URL
+      : process.env.REACT_APP_PROD_API_URL;
   const info = await Promise.all([
-    fetch(
-      `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/${id}`,
-      requestOptions
-    ).then((response) => response.json()),
-    fetch(
-      `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/${id}/reviews`,
-      requestOptions
-    ).then((response) => response.json()),
+    fetch(`${url}/api/stylist/${id}`).then((response) => response.json()),
+    fetch(`${url}/api/reviews/${id}`).then((response) => response.json()),
   ]);
   return info;
 };
 
 export const getSearchResults = async (terms) => {
+  const url =
+    process.env.NODE_ENV !== "production"
+      ? process.env.REACT_APP_DEV_API_URL
+      : process.env.REACT_APP_PROD_API_URL;
+  console.log(process.env.REACT_APP_DEV_API_URL, "dev url");
+  console.log(process.env.NODE_ENV, "environment");
   const searchTerm = terms.style.split(" ").join("+");
   const info = await fetch(
-    `http://localhost:3001/api/search?term=${searchTerm}&location=${terms.location}`
+    `${url}/api/search?term=${searchTerm}&location=${terms.location}`
   );
   const data = await info.json();
   return data;
